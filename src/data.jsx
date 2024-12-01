a1a6caa4f772499492a9cf7fd06a38d3411adc84
Sapnish-S <sapnishsharma111@gmail.com>
2024-12-01 17:11:07 -0600

    added some data in here

--- src/data.jsx
+++ src/data.jsx
@@ -1,17 +1,17 @@
 export const tutorscard=[
     {
         id:1,
-        user:"Prayanshu Pradhan",
-        desc: "Computer Science tutor" ,
+        user:"Pra. Pradhan",
+        desc: "Comp. Science tutor" ,
         img:  './img/data1.webp',
-        charge: "$10/hr"
+        charge: "$15/hr"
     },
     {
         id:2,
         user:"Smera Shrestha",
         desc: "Mathematics tutor" ,
         img:  'https://media.licdn.com/dms/image/v2/D4E03AQECD0Vr24L6MA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1718786676297?e=1736985600&v=beta&t=TA-HLCc_ubaCAOJU79zOznDi2VBrbzwCYHcCyrOpY3g',
-        charge: "$10/hr"
+        charge: "$15.5/hr"
     
     },  
     {
@@ -19,7 +19,7 @@ export const tutorscard=[
         user:"Hayden Eddy",
         desc: "Mathematics tutor" ,
         img:  './img/data1.webp',
-        charge: "$10/hr"
+        charge: "$14/hr"
     },  
     {
         id:4,
@@ -31,22 +31,28 @@ export const tutorscard=[
     {
         id:5,
         user:"Nima Lama",
-        desc: "Computer Science tutor" ,
-        img:  'https://scontent.fmci1-3.fna.fbcdn.net/v/t39.30808-6/452442464_4550403255184833_2526817635490630328_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=127cfc&_nc_ohc=XCj5P4EtcQ4Q7kNvgEI60lu&_nc_zt=23&_nc_ht=scontent.fmci1-3.fna&_nc_gid=AYGy5fywikW-6pRmDHebr_O&oh=00_AYDCbIbTWmMEv3vbopnF1DyCeDcFNMvR8TXBE-UV925GBg&oe=673DC93C',
-        charge: "$10/hr"
+        desc: "Comp. Science tutor" ,
+        img:  './img/nima.jpg',
+        charge: "$17/hr"
     },  
     {
         id:6,
         user:"Aayush Nakarmi",
-        desc: "Computer Science tutor" ,
-        img:  'https://scontent.fmci1-4.fna.fbcdn.net/v/t39.30808-6/347226006_3513647425570070_2002811752204318257_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=-V0hLCENFD0Q7kNvgHPXBCu&_nc_zt=23&_nc_ht=scontent.fmci1-4.fna&_nc_gid=AHP4A2llBBmz7N8-bs-8yMg&oh=00_AYBOC3Ys_KEzrfxp3sjDV1V0xZuHDH6jwOgoltIQXxXyfw&oe=673DC657',
-        charge: "$10/hr"
+        desc: "Comp. Science tutor" ,
+        img:  './img/aayush.jpg',
+        charge: "$16/hr"
     },  
     {
         id:7,
         user:"Ujjwal Sitaula",
         desc: "Washburn Experience tutor" ,
-        img:  'https://scontent.fmci1-3.fna.fbcdn.net/v/t39.30808-6/424888339_2441742042700788_2900288641589587232_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=127cfc&_nc_ohc=DD98pbTVz4UQ7kNvgFNV1ak&_nc_zt=23&_nc_ht=scontent.fmci1-3.fna&_nc_gid=A216PFX0JrHhUiTrbi9PyNE&oh=00_AYDI_t_rEbxSecA0DF986z4biS6_lGBaqJRZp1Tqcwh4SA&oe=673DAA31',
+        img:  './img/uzzwal.jpg',
         charge: "$7.25/hr"
-    },
+    }, {
+        id:7,
+        user:"Dol Gurung",
+        desc: " Football Coach" ,
+        img:  './img/dol.jpg',
+        charge: "$7.25/hr"
+    }
 ]
