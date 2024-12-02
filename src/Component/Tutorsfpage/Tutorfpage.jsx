78f17bd4a0484c8e8b924b9d3ad4d5e182e3770d
Sapnish-S <sapnishsharma111@gmail.com>
2024-12-01 20:16:06 -0600

    Updated Home page, tutorfpage and added tutor registration

--- src/Component/Tutorsfpage/Tutorfpage.jsx
+++ src/Component/Tutorsfpage/Tutorfpage.jsx
@@ -1,39 +1,39 @@
-import React from 'react'
-import './Tutorfpage.css'
+import React from 'react';
+import './Tutorfpage.css';
 import { FaCheckSquare } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 const Tutorfpage = () => {
   return (
     <div className='tutorfpage'>
-        <div className='tutor-features'>
-                <div className='container'>
-                    <div className="item">
-                    <h1>Share Your Knowledge, Shape the Future.</h1>
-                    <h1>Be a Tutor Today!</h1>
-                    <p>Make a difference in a student's life.</p>
-                        <div className="title">
-                            <FaCheckSquare size={20}/> Professional Development Opportunities
-                            </div>
-                            <p>Access resources to enhance your teaching skills.</p>
-                        <div className="title">
-                            <FaCheckSquare size={20}/> Flexible Scheduling
-                            </div>
-                            <p>Choose your own hours and work-life balance.</p>
-                        <div className="title">
-                            <FaCheckSquare size={20}/> Competitive Earnings
-                            </div>
-                            <p>Earn a rewarding income for your expertise..</p>
-                            </div>
-                            
-                            </div>
-                        
-                        <div className="item">
-                        <img src="./img/tutor.jpg" alt="" />
-                        </div>
-                        
-                </div>
-                <button className='btn'>Become a Maestro</button>
+      <div className='tutor-features'>
+        <div className='container'>
+          <div className='description'>
+            <h1>Share Your Knowledge, Shape the Future.</h1>
+            <h1>Be a Tutor Today!</h1>
+            <p>Make a difference in a student's life.</p>
+            <div className='title'>
+              <FaCheckSquare size={20}/> Professional Development Opportunities
+            </div>
+            <p>Access resources to enhance your teaching skills.</p>
+            <div className='title'>
+              <FaCheckSquare size={20}/> Flexible Scheduling
+            </div>
+            <p>Choose your own hours and work-life balance.</p>
+            <div className='title'>
+              <FaCheckSquare size={20}/> Competitive Earnings
+            </div>
+            <p>Earn a rewarding income for your expertise.</p>
+          </div>
+          <div className='image'>
+            <img src="./img/tutor.jpg" alt="Tutor" />
+          </div>
+        </div>
+        <Link to="/Tutorregistration" className="link">
+        <button className='btn'>Become a Maestro</button> </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Tutorfpage
+export default Tutorfpage;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -12,27 +12,24 @@ const Home = () => {
         <Featured/>
         <Toptutor/>
         <div className='features'>
-                <div className='container'>
-                    <div className="item">
-                    <h1>Your Future Starts Here</h1>
-                    
-                    <p>Sign up now and unlock your potential.</p>
-                        <div className="title">
-                            <FaCheckSquare size={20}/> Expert Tutors
-                            </div>
-                            <p>Access qualified tutors from your university.</p>
-                        <div className="title">
-                            <FaCheckSquare size={20}/> Flexible Scheduling
-                            </div>
-                            <p> Book sessions at your convenience.</p>
-                        <div className="title">
-                            <FaCheckSquare size={20}/> Personalized Learning
-                            </div>
-                            <p>Get tailored support for your specific needs.</p>
-                            </div>
-                            
-                            </div>
-                        
+            <div className='container'>
+          <div className='description'>
+          <h1>Your Future Starts Here</h1>
+          <p>Sign up now and unlock your potential.</p>
+            <div className='title'>
+              <FaCheckSquare size={20}/> Expert Tutors
+            </div>
+            <p>Access qualified tutors from your university.</p>
+            <div className='title'>
+              <FaCheckSquare size={20}/> Flexible Scheduling
+            </div>
+            <p>Book sessions at your convenience.</p>
+            <div className='title'>
+              <FaCheckSquare size={20}/> Personalized Learning
+            </div>
+            <p>Get tailored support for your specific needs.</p>
+          </div>
+         </div>  
                         <div className="item">
                         <video src="./img/Maestrominds.mp4" controls></video>
                         </div>
